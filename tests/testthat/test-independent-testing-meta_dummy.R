5dfacdb528806ff4413962e09814b14288985c2b
Wang, Benjamin <benjamin.wang@merck.com>
2022-07-20 09:43:20 -0400

    test case for meta_dummy

---
+++ tests/testthat/test-independent-testing-meta_dummy.R
@@ -0,0 +1,13 @@
+
+x <- meta_dummy()
+
+test_that("meta_dummy() structure", {
+  expect_equal(class(x), "meta_adam")
+  expect_equal(class(x$data_population), "data.frame")
+  expect_equal(class(x$data_observation), "data.frame")
+  expect_equal(class(x$population), "list")
+  expect_equal(class(x$observation), "list")
+  expect_equal(class(x$parameter), "list")
+  expect_equal(class(x$analysis), "list")
+  
+})
