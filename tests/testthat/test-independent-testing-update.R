2a5818f848643faf2a34ccb2d9764cb5fbc15fc9
Nepal, Sarad <sarad.nepal@merck.com>
2022-08-31 09:15:47 -0400

    independent testing update R.

---
+++ tests/testthat/test-independent-testing-update.R
@@ -0,0 +1,11 @@
+
+test_that("update adam mapping with additional variable AEREL in existing adam mapping", { 
+  meta1 <- update_adam_mapping(meta_dummy(), "rel", start_date = "ASTDT", stop_date = "ASTDT" )
+  expect_equal(names(collect_adam_mapping(meta1, "rel")), c("name","id","group","var","subset","label","soc","seq","term1","term2","summ_row","summ_foot",".location","start_date","stop_date"))
+}) 
+
+
+test_that("update adam mapping with additional variable ser in existing adam mapping", { 
+  meta2 <-  update_adam_mapping(meta_dummy(), "ser", clinint="Y")
+  expect_equal(names(collect_adam_mapping(meta2, "ser")), c("name","id","group","var","subset","label","soc","seq","term1","term2","summ_row",".location","clinint"))
+}) 
