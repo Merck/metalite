d4b2ae72c059dd4d19f741925b4078a0309ca41c
LittleBeannie <yujie.zhao@merck.com>
2022-09-08 18:15:52 -0400

    add label functions

---
+++ R/label.R
@@ -0,0 +1,62 @@
+#' A function to get the labels of data frame columns
+#'
+#' @param data a data frame
+#'
+#' @return labels of the input data frame
+#'
+#' @examples 
+#' get_label(r2rtf::r2rtf_adae)
+#' 
+get_label <- function(data){
+  
+  label <- vapply(data, function(x){
+    if(is.null(attr(x, "label"))){
+      return(NA_character_)
+    }else{
+      attr(x, "label")
+    }
+  }, FUN.VALUE = character(1))
+  
+  ifelse(is.na(label), names(data), label)
+  
+}
+
+#' A function to assign labels to a data frame 
+#'
+#' @param data a data frame
+#' @param var the variables to assign labels
+#' @param label the labels to be assigned
+#'
+#' @return a data frame with labels updated 
+#'
+#' @examples
+#' assign_label(r2rtf::r2rtf_adae)
+#' assign_label(r2rtf::r2rtf_adae, var = "USUBJID", label = "Unique subject id")
+assign_label <- function(data,
+                         var = names(data),
+                         label = names(data)){
+  
+  # input checking
+  stopifnot(length(var) == length(label))
+  stopifnot(! any(duplicated(var)))
+  
+  # check missing label
+  name <- names(data)
+  diff <- setdiff(name, var)
+  
+  if(length(diff) > 0){
+    message("missing variables set label as itself\n",
+            paste(diff, collapse = "\n"))
+    
+    var <- c(var, diff)
+    label <- c(label, diff)
+  }
+  
+  # assign label
+  for(i in seq(name)){
+    attr(data[[i]], "label") <- label[names(data[i]) == var]
+  }
+  
+  data
+  
+}
