0e9ab022194de9ca3bf354a99f1572db0fcf8f01
ginnaram <madhusudhan.ginnaram.reddy@merck.com>
2022-09-14 14:32:22 -0400

    meta_split validation updates

--- tests/testthat/test-independent-testing-meta_split.R
+++ tests/testthat/test-independent-testing-meta_split.R
@@ -8,40 +8,40 @@ test_that("variable 'RACE' checking", {
 test_that("variable 'RACE = WHITE' checking in data_population", {
   x <- meta_dummy() |> meta_split("RACE")
   z <- meta_dummy()$data_population %>% filter(RACE=="WHITE")
-  expect_equal(length(x$WHITE$data_population$USUBJID), length(z$USUBJID))
+  expect_equal(x$WHITE$data_population$USUBJID, unlist(as.list(z$USUBJID)))
 }) 
 
 
 test_that("variable 'RACE = BLACK OR AFRICAN AMERICAN' checking in data_population", {
   x <- meta_dummy() |> meta_split("RACE")
   z <- meta_dummy()$data_population %>% filter(RACE=="BLACK OR AFRICAN AMERICAN")
-  expect_equal(length(x$`BLACK OR AFRICAN AMERICAN`$data_population$USUBJID), length(z$USUBJID))
+  expect_equal(x$`BLACK OR AFRICAN AMERICAN`$data_population$USUBJID, unlist(as.list(z$USUBJID)))
 }) 
 
 
 test_that("variable 'RACE = AMERICAN INDIAN OR ALASKA NATIVE' checking in data_population", {
   x <- meta_dummy() |> meta_split("RACE")
   z <- meta_dummy()$data_population %>% filter(RACE=="AMERICAN INDIAN OR ALASKA NATIVE")
-    expect_equal(length(x$`AMERICAN INDIAN OR ALASKA NATIVE`$data_population$USUBJID), length(z$USUBJID))
+    expect_equal(x$`AMERICAN INDIAN OR ALASKA NATIVE`$data_population$USUBJID, unlist(as.list(z$USUBJID)))
 }) 
 
 
 test_that("variable 'RACE = WHITE' checking in data_observation", {
   x <- meta_dummy() |> meta_split("RACE")
   z <- meta_dummy()$data_observation %>% filter(RACE=="WHITE")
-  expect_equal(length(x$WHITE$data_observation$USUBJID), length(z$USUBJID))
+  expect_equal(x$WHITE$data_observation$USUBJID, unlist(as.list(z$USUBJID)))
 }) 
 
 
 test_that("variable 'RACE = BLACK OR AFRICAN AMERICAN' checking in data_observation", {
   x <- meta_dummy() |> meta_split("RACE")
   z <- meta_dummy()$data_observation %>% filter(RACE=="BLACK OR AFRICAN AMERICAN")
-  expect_equal(length(x$`BLACK OR AFRICAN AMERICAN`$data_observation$USUBJID), length(z$USUBJID))
+  expect_equal(x$`BLACK OR AFRICAN AMERICAN`$data_observation$USUBJID, unlist(as.list(z$USUBJID)))
 }) 
 
 
 test_that("variable 'RACE = AMERICAN INDIAN OR ALASKA NATIVE' checking in data_observation", {
   x <- meta_dummy() |> meta_split("RACE")
   z <- meta_dummy()$data_observation %>% filter(RACE=="AMERICAN INDIAN OR ALASKA NATIVE")
-  expect_equal(length(x$`AMERICAN INDIAN OR ALASKA NATIVE`$data_observation$USUBJID), length(z$USUBJID))
+  expect_equal(x$`AMERICAN INDIAN OR ALASKA NATIVE`$data_observation$USUBJID, unlist(as.list(z$USUBJID)))
 }) 
