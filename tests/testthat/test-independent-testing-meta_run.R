8174e0ccb57c71acf419d163403c36fed4ed680a
Howard Baek <howard.baek@merck.com>
2022-10-05 13:51:13 -0400

    Option2: Put ae_summary() and ae_specific inside each 3 test_that() environment

--- tests/testthat/test-independent-testing-meta_run.R
+++ tests/testthat/test-independent-testing-meta_run.R
@@ -1,22 +1,36 @@
-ae_summary <- function(...) {
-  paste("results of", deparse(match.call(), nlines = 1))
-}
-ae_specific <- function(...) {
-  paste("results of", deparse(match.call(), nlines = 1))
-}
-
 # Create dummy meta_adam object
 meta <- meta_dummy()
 
 # Snapshot testing
 test_that("all analysis based on the analysis plan would be executed", {
+  ae_summary <- function(...) {
+    paste("results of", deparse(match.call(), nlines = 1))
+  }
+  ae_specific <- function(...) {
+    paste("results of", deparse(match.call(), nlines = 1))
+  }
+  
   expect_snapshot(meta_run(meta))
 })
 
 test_that("only the first analysis based on the analysis plan would be executed", {
+  ae_summary <- function(...) {
+    paste("results of", deparse(match.call(), nlines = 1))
+  }
+  ae_specific <- function(...) {
+    paste("results of", deparse(match.call(), nlines = 1))
+  }
+  
   expect_snapshot(meta_run(meta, i = 1))
 })
 
 test_that("selected analysis based on the analysis plan would be executed", {
+  ae_summary <- function(...) {
+    paste("results of", deparse(match.call(), nlines = 1))
+  }
+  ae_specific <- function(...) {
+    paste("results of", deparse(match.call(), nlines = 1))
+  }
+  
   expect_snapshot(meta_run(meta, i = c(1, 3, 5)))
 })
