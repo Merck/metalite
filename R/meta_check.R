a579a058f13880d6f00eb2576f6458a63e656170
Zhao, Yujie <yujie.zhao@merck.com>
2023-01-31 13:58:53 -0500

    fix cmd check

--- R/meta_check.R
+++ R/meta_check.R
@@ -22,7 +22,7 @@
 #' @param type a character vector of data type to check
 #' @returns nothing is check is passed; otherwise some error message
 #' @examples
-#' meta_check_var(meta_dummy(), var = "RACE")
+#' metalite:::eta_check_var(meta_dummy(), var = "RACE")
 meta_check_var <- function(meta,
                            var,
                            type = c("population", "observation")) {
