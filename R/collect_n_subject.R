2f3c6bb1b22c5921011c5a7ee4c785fe0764a8ee
Wang, Benjamin <benjamin.wang@merck.com>
2023-07-17 21:41:41 -0400

    add argument na_name

--- R/collect_n_subject.R
+++ R/collect_n_subject.R
@@ -23,6 +23,7 @@
 #' @param par A character vector of parameter name.
 #' @param use_na A character value for whether to include `NA` values
 #'   in the table. See the `useNA` argument in [base::table()] for more details.
+#' @param na_name A character value for missing value label, default is "Missing".
 #'
 #' @return A data frame summarizing the number of unique subjects
 #'   in different arms.
@@ -38,14 +39,16 @@
 #' n_subject(r2rtf_adae$USUBJID, r2rtf_adae$TRTA)
 #' n_subject(r2rtf_adae$USUBJID, r2rtf_adae$TRTA, r2rtf_adae$SEX)
 #' n_subject(r2rtf_adae$USUBJID, r2rtf_adae$TRTA, r2rtf_adae$SEX, use_na = "always")
+#' n_subject(r2rtf_adae$USUBJID, r2rtf_adae$TRTA, r2rtf_adae$SEX, na_name = "Null")
 n_subject <- function(id,
                       group,
                       par = NULL,
-                      use_na = c("ifany", "no", "always")) {
+                      use_na = c("ifany", "no", "always"),
+                      na_name = "Missing") {
   use_na <- match.arg(use_na)
 
   if ("factor" %in% class(group)) {
-    u_group <- c(as.character(levels(group)), "Missing")
+    u_group <- c(as.character(levels(group)), na_name)
   } else {
     stop("n_subject: group variable must be a factor")
   }
@@ -61,7 +64,7 @@ n_subject <- function(id,
     db <- data.frame(id = id, group = group, par = par)
     res <- table(unique(db)[, c("group", "par")], useNA = use_na)
     name <- colnames(res)
-    name[is.na(name)] <- "Missing"
+    name[is.na(name)] <- na_name
 
     n_row <- nrow(res)
     n_col <- ncol(res)
