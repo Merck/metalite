a56777bce6696b9351780cb024481f2c9f9a7979
Nan Xiao <me@nanx.me>
2024-02-13 22:24:10 -0500

    Remove duplicated helper functions

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -5,11 +5,3 @@ ae_summary <<- function(...) {
 ae_specific <<- function(...) {
   paste("results of", deparse(match.call(), nlines = 1))
 }
-
-ae_summary <<- function(...) {
-  paste("results of", deparse(match.call(), nlines = 1))
-}
-
-ae_specific <<- function(...) {
-  paste("results of", deparse(match.call(), nlines = 1))
-}
